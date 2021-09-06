a5d913e0a7907e80dd63049f5abaca9e42e3546a
孙素平 <spsun@streamax.com>
2021-09-06 10:02:00 +0800

    Initial Commit

---
+++ SwiftModuleDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftModuleDemo
+//
+//  Created by ssp on 2021/9/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
