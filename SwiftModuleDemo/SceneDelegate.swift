54e2ad1b96a04b619a942e0f32a6e0ed0168e53b
孙素平 <spsun@streamax.com>
2021-09-09 14:57:25 +0800

    初始化
    1.删除main.storyboard
    2.增加launchScreen

--- SwiftModuleDemo/SceneDelegate.swift
+++ SwiftModuleDemo/SceneDelegate.swift
@@ -19,7 +19,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
        
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+            
+            window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+            window?.windowScene = windowScene
+            window?.backgroundColor = UIColor.white
+            window?.makeKeyAndVisible()
+            window?.rootViewController = UITabBarController()
     }
 
     @available(iOS 13.0, *)
